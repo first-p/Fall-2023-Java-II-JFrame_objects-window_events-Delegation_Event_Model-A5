4f14788fcc86121472ede52692f6c97310c592a5
first-p <98608650+first-p@users.noreply.github.com>
2023-11-09 14:02:12 -0600

    add skeleton code to get JFrame basic functionality

--- Proj05Runner.java
+++ Proj05Runner.java
@@ -1,2 +1,36 @@
+/****************************************
+ File: Proj05runner.java
+ Author: Fred Butoma
+ Purpose: Proj05Runner.java is a class that implements a JFrame window
+          and simple functional user interation
+ ****************************************/
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.WindowConstants;
+
 public class Proj05Runner {
+
+    //Constructor for Proj05Runner class
+    public Proj05Runner() {}
+    System.out.println(" I certify that this program is my own work\n" +
+                        "and is not the work of others. I agree not\n" +
+                        "to share my solutions with others.\n" +
+                        "-Fred Butoma");
 }
+
+//Create a JFrame window
+
+//Set the JFrame window to 300 pixels wide and 100 pixels tall
+
+//Set the default close operation to close the program when the
+// user clicks the close button
+
+//Create a JLabel which will be displayed inside the JFrame
+
+//Set the JFrame location to the upper-left corner of the screen
+
+//Make the JFrame visible to the user
+
+//Add a WindowListener to handle window events
+
